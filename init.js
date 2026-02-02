6a7f880eeb3aa6e77832cac8e821958b5e696a5d
tom_glob <544j544j@gmail.com>
2026-02-03 00:26:24 +0800

    777

--- init.js
+++ init.js
@@ -1,6 +1,4 @@
-<script>
-    // Google Apps Script Web App URL
-    // 請部署 Google Apps Script 後，將 Web App URL 填入下方
-    const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwpoW8jgSJC-_57xFmBh-zrcVBHtafhVPK3WMNpuevss290lqC7K4-7TYDw_xpGzNI/exec';
-        
-</script>
+
+// Google Apps Script Web App URL
+// 請部署 Google Apps Script 後，將 Web App URL 填入下方
+const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwpoW8jgSJC-_57xFmBh-zrcVBHtafhVPK3WMNpuevss290lqC7K4-7TYDw_xpGzNI/exec';
