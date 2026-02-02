e1ca4af8ff0faad975cd339d52f52a711bcb661e
tom_glob <544j544j@gmail.com>
2026-02-03 00:30:39 +0800

    8

--- init.js
+++ init.js
@@ -1,4 +1,4 @@
 
 // Google Apps Script Web App URL
 // 請部署 Google Apps Script 後，將 Web App URL 填入下方
-const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwpoW8jgSJC-_57xFmBh-zrcVBHtafhVPK3WMNpuevss290lqC7K4-7TYDw_xpGzNI/exec';
+const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbznTIniVo6fe10FVTDodBctTBDHUPUUAkC-FJKG_vIQz-1dCtKxxfaorWfWez3c0T_S/exec';
